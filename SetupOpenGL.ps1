9d0ed277fd6aad01b9e17fa685829cfad6b86c53
Shishir Ahmed <51795191+shishirahm3d@users.noreply.github.com>
2025-01-27 19:56:14 +0600

    Update SetupOpenGL.ps1

--- SetupOpenGL.ps1
+++ SetupOpenGL.ps1
@@ -1,5 +1,5 @@
 # Define URLs for downloads
-$codeBlocksURL = "https://www.fosshub.com/Code-Blocks.html?dwl=codeblocks-20.03-setup.exe"
+$codeBlocksWithMinGWURL = "https://www.fosshub.com/Code-Blocks.html?dwl=codeblocks-20.03mingw-setup.exe"
 $openGLURL = "http://freeglut.sourceforge.net/index.html"  # Update with a direct download link if possible
 
 # Define installation paths
@@ -17,12 +17,12 @@ function Download-File {
     Write-Host "Downloaded to $destination."
 }
 
-# Download and install Code::Blocks
-Write-Host "Installing Code::Blocks..."
+# Download and install Code::Blocks with MinGW
+Write-Host "Installing Code::Blocks with MinGW..."
 $codeBlocksInstaller = "$env:TEMP\codeblocks-setup.exe"
-Download-File -url $codeBlocksURL -destination $codeBlocksInstaller
+Download-File -url $codeBlocksWithMinGWURL -destination $codeBlocksInstaller
 Start-Process -FilePath $codeBlocksInstaller -ArgumentList "/S" -Wait  # Silent install
-Write-Host "Code::Blocks installed at $installPath."
+Write-Host "Code::Blocks with MinGW installed at $installPath."
 
 # Download and set up OpenGL/GLUT
 Write-Host "Setting up OpenGL/GLUT..."
@@ -31,19 +31,19 @@ $openGLZip = "$env:TEMP\opengl.zip"
 Download-File -url $openGLURL -destination $openGLZip
 Expand-Archive -Path $openGLZip -DestinationPath $openGLPath -Force
 
-# Configure Code::Blocks
-Write-Host "Configuring Code::Blocks..."
+# Configure Code::Blocks to use the MinGW compiler
+Write-Host "Configuring Code::Blocks to use MinGW..."
 $codeBlocksUserDir = "$env:APPDATA\codeblocks"
 $compilerOptions = @"
 [Compiler]
-Add="-I$openGLPath\include"
-Add="-L$openGLPath\lib"
+Add="-IC:\OpenGL\include"
+Add="-LC:\OpenGL\lib"
 Add="-lglu32 -lopengl32 -lfreeglut"
 "@
 $configFile = Join-Path $codeBlocksUserDir "default.conf"
 if (Test-Path $configFile) {
     $compilerOptions | Out-File -Append -Encoding UTF8 $configFile
-    Write-Host "OpenGL configuration added to Code::Blocks!"
+    Write-Host "OpenGL and MinGW configuration added to Code::Blocks!"
 } else {
     Write-Host "Code::Blocks configuration file not found!"
 }
